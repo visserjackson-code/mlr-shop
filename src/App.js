7867f1817c1d1f8cc6f999813f895d6a5fd4dc09
visserjackson-code <visserjackson@gmail.com>
2026-01-28 09:32:15 -0500

    new tab

--- src/App.js
+++ src/App.js
@@ -418,7 +418,7 @@ Most Likely Records is a curated vinyl shop run by me,
     marginBottom: '20px',
     color: colors.orange
   }}>
-    <a href="https://youtu.be/vwzso2qaFgo" style={{ color: colors.orange, textDecoration: 'none' }}>LATEST VIDEO</a>
+    <a href="https://youtu.be/vwzso2qaFgo" target='_blank' rel='noreferrer' style={{ color: colors.orange, textDecoration: 'none' }}>LATEST VIDEO</a>
   </h2>
   
   <div style={{
